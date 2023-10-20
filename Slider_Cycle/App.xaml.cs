284a8cbfb4dfe16ed0b13eef09eecd4581ceaaa8
Mahdi Rastegari Koopaei <mrastegari@basemap.co.uk>
2023-10-20 17:45:29 +0100

    Replicate the bug

---
+++ Slider_Cycle/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Slider_Cycle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Slider_Cycle/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Slider_Cycle
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
